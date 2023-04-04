cff3ac208e70683e11960c5cf95e0d33c70cc254
Connor Kozlowski <connor.kozlowski@slu.edu>
2023-04-03 22:31:21 -0500

    UI prototype

--- app/src/main/java/SIgame/view/SpaceGUI.java
+++ app/src/main/java/SIgame/view/SpaceGUI.java
@@ -39,40 +39,80 @@ public class SpaceGUI
         drawGameScreen(difficulty);
     }
 
-    public void drawMainMenu()
-    {
-        //imageDrawing("/SIgame/resources/logo.png", titleScreen, 1);
-
-        ImageIcon icon = new ImageIcon(getClass().getClassLoader().getResource("alien.png"));
+    public void drawMainMenu() {
+        ImageIcon logo = new ImageIcon(getClass().getClassLoader().getResource("logo.png"));
         menuFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        JLabel image = new JLabel(icon);
+        JLabel image = new JLabel(logo);
         titleScreen.setPreferredSize(new Dimension(640, 480));
         titleScreen.setBackground(Color.BLACK);
         titleScreen.setLayout(new BoxLayout(titleScreen, BoxLayout.PAGE_AXIS));
-
-        //Graphics.drawImage(image,100,100);
-        menuFrame.add(image);
-        //menuFrame.add(titleScreen);
+        JPanel labelPanel = new JPanel();
+        labelPanel.setBackground(Color.BLACK);
+        labelPanel.setLayout(new BoxLayout(labelPanel, BoxLayout.PAGE_AXIS));
+        JLabel label = new JLabel("Select Difficulty:");
+        label.setForeground(Color.WHITE);
+        label.setFont(new Font("Arial", Font.BOLD, 24));
+        JLabel normalLabel = new JLabel("[ Normal ]");
+        normalLabel.setForeground(Color.WHITE);
+        normalLabel.setFont(new Font("Arial", Font.PLAIN, 18));
+        JLabel hardLabel = new JLabel("Hard");
+        hardLabel.setForeground(Color.WHITE);
+        hardLabel.setFont(new Font("Arial", Font.PLAIN, 18));
+        labelPanel.add(Box.createVerticalGlue());
+        labelPanel.add(label);
+        labelPanel.add(Box.createRigidArea(new Dimension(0, 20)));
+        labelPanel.add(normalLabel);
+        labelPanel.add(Box.createRigidArea(new Dimension(0, 20)));
+        labelPanel.add(hardLabel);
+        labelPanel.add(Box.createVerticalGlue());
+        titleScreen.add(image);
+        titleScreen.add(labelPanel);
+        menuFrame.add(titleScreen);
         menuFrame.pack();
         menuFrame.setVisible(true);
     }
 
-    public void drawGameScreen(String difficulty)
-    {
-        //JLabel label = new JLabel("GAME");
-        //label.setOpaque(true);
-        gameFrame = new JFrame("difficulty");
-        gameFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    
 
+    public void drawGameScreen(String difficulty) {
+        int alienStartX = 50;
+        gameFrame = new JFrame(difficulty);
+        gameFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    
         gameScreen.setBackground(Color.BLACK);
         gameScreen.setPreferredSize(new Dimension(640, 480));
-        gameScreen.setLayout(new BoxLayout(gameScreen, BoxLayout.PAGE_AXIS));
+        gameScreen.setLayout(null);
 
-        ImageIcon test2 = new ImageIcon(getClass().getClassLoader().getResource("logo.png"));
-        JLabel gameScreen = new JLabel(test2);
-        
+        JLabel scoreLabel = new JLabel("Score: 5");
+        scoreLabel.setForeground(Color.WHITE);
+        scoreLabel.setBounds(20, 10, 80, 30);
+        gameScreen.add(scoreLabel);
+        JLabel highScoreLabel = new JLabel("High Score: 35");
+        highScoreLabel.setForeground(Color.WHITE);
+        highScoreLabel.setBounds(120, 10, 100, 30);
+        gameScreen.add(highScoreLabel);
+        JLabel livesLabel = new JLabel("Lives: 2");
+        livesLabel.setForeground(Color.WHITE);
+        livesLabel.setBounds(250, 10, 80, 30);
+        gameScreen.add(livesLabel);
+        ImageIcon tankIcon = new ImageIcon(getClass().getClassLoader().getResource("tank.png"));
+        Image scaledTank = tankIcon.getImage().getScaledInstance(60, 60, Image.SCALE_DEFAULT);
+        tankIcon = new ImageIcon(scaledTank);
+        JLabel tankLabel = new JLabel(tankIcon);
+        tankLabel.setBounds(290, 420, 60, 60);
+        gameScreen.add(tankLabel);
+             
+        for (int i = 0; i < 6; i++) 
+        {
+            ImageIcon alienIcon = new ImageIcon(getClass().getClassLoader().getResource("alien.png"));
+            Image scaledAlien = alienIcon.getImage().getScaledInstance(40, 40, Image.SCALE_DEFAULT);
+            alienIcon = new ImageIcon(scaledAlien);
+            JLabel alienLabel = new JLabel(alienIcon);
+            alienLabel.setBounds(alienStartX + (i * 50), 50, 40, 40);
+            gameScreen.add(alienLabel);
+        }
+    
         gameFrame.add(gameScreen);
-        //gameFrame.add(label);
         gameFrame.pack();
         gameFrame.setVisible(true);
     }
@@ -107,6 +147,12 @@ public class SpaceGUI
 
         targetPanel.add(image);
     }
+    
+    public void resize(ImageIcon image){
+
+
+    }
+
     
     
 
